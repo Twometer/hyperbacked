e27c75d99a2e6d23fe3fe5bb8a31a6f692317905
Twometer <twometer@outlook.de>
2022-12-24 16:21:58 +0100

    Simple Shamir secret sharing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use passphrase::gen_passphrase;
+use sharks::{Share, Sharks};
 
 use crate::crypto::{decrypt_secret, encrypt_secrets, Secret};
 
@@ -32,8 +33,23 @@ fn main() -> anyhow::Result<()> {
     println!("Passphrase 3: {}", pass3);
 
     let secret = encrypt_secrets(secrets)?;
-    fs::write("./test.bin", &secret)?;
 
+    let sharks = Sharks(4); // min required 4
+    let dealer = sharks.dealer(&secret);
+
+    // Create 7 shares
+    let shares: Vec<Share> = dealer.take(7).collect();
+    let mut ctr = 0;
+    for share in &shares {
+        let sharebin = Vec::from(share);
+        ctr += 1;
+        fs::write(format!("test-share-{}.bin", ctr), sharebin)?;
+    }
+
+    // Recover the secret from 4 shares
+    let secret = sharks.recover(&shares[0..=3]).unwrap();
+
+    // And let's see if we can decode it again
     println!("Decoded 1: {}", decrypt_secret(&secret, &pass1)?);
     println!("Decoded 2: {}", decrypt_secret(&secret, &pass2)?);
     println!("Decoded 3: {}", decrypt_secret(&secret, &pass3)?);
