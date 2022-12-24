0a6cceb0f8699dbd648e942dac929544539a7c98
Twometer <twometer@outlook.de>
2022-12-24 15:52:48 +0100

    Unit test passphrase generator

--- src/passphrase.rs
+++ src/passphrase.rs
@@ -15,3 +15,21 @@ pub fn gen_passphrase(num_words: usize) -> String {
         .collect::<Vec<&str>>()
         .join(&" ");
 }
+
+#[cfg(test)]
+mod test {
+    use super::gen_passphrase;
+
+    #[test]
+    fn test_word_count() {
+        let passphrase = gen_passphrase(4);
+        assert_eq!(4, passphrase.split(" ").count());
+    }
+
+    #[test]
+    fn test_different_results() {
+        let passphrase1 = gen_passphrase(4);
+        let passphrase2 = gen_passphrase(4);
+        assert_ne!(passphrase1, passphrase2);
+    }
+}
